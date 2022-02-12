46ecd068b72c7d6dcca35a324e97be50799e812f
Julien Zapata Duque <jzapata@live.nl>
2022-02-12 12:32:31 +0100

    feat: 🚀 abstract prisma client through repositories

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { UsersModule } from './users/users.module';
+import { OrmModule } from './orm/orm.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, OrmModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ src/orm/repositories/users.repository.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../../prisma.service';
+
+export type UserCreateInput = Prisma.UserCreateInput;
+
+@Injectable()
+export class UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  public create(data: UserCreateInput) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  get find() {
+    return this.prisma.user.findMany;
+  }
+
+  findOne(id: number) {
+    return this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  get delete() {
+    return this.prisma.user.delete;
+  }
+
+  get deleteMany() {
+    return this.prisma.user.deleteMany;
+  }
+}
