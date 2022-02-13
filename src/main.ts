923136180ff75ce0dc05fcafcff0911d16812d79
Julien Zapata Duque <jzapata@live.nl>
2022-02-13 13:26:53 +0100

    feat: 🚀 use global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ProtectFieldsPipe } from './shared/pipes/protect-fields.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,13 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      validateCustomDecorators: true,
+      transform: true,
+    }),
+    new ProtectFieldsPipe()
+  );
   await app.listen(3000);
 }
 bootstrap();
