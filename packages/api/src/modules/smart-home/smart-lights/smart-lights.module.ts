bf7b247568449024ddd40b33f8a66ffff991e456
Julien Zapata Duque <jzapata@live.nl>
2022-08-14 22:10:47 +0200

    refactor(api): retrieve tradfri credentials from env

--- packages/api/src/modules/smart-home/smart-lights/smart-lights.module.ts
+++ packages/api/src/modules/smart-home/smart-lights/smart-lights.module.ts
@@ -1,15 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TradfriService } from './providers/tradfri.service';
 import { SmartLightsController } from './smart-lights.controller';
 import { ISmartLightsService } from './smart-lights.service';
 
-// TODO: Convert into a dynamic light service resolver using a factory
-const tempTradfriServiceProvider = {
+// TODO: Convert into a dynamic light service resolver using a factory based on home settings
+const tempTradfriServiceProvider: Provider<ISmartLightsService> = {
   provide: ISmartLightsService,
-  useValue: new TradfriService({} as never), // TODO: Pass correct config
+  useFactory: (configService: ConfigService) => {
+    const securityCode = configService.get('TRADFRI_SECURITY_CODE');
+    const psk = configService.get('TRADFRI_PSK');
+    const identity = configService.get('TRADFRI_IDENTITY');
+
+    return new TradfriService({
+      securityCode,
+      psk,
+      identity,
+    });
+  },
+  inject: [ConfigService],
 };
 
 @Module({
+  imports: [ConfigModule],
   providers: [tempTradfriServiceProvider],
   controllers: [SmartLightsController],
 })
