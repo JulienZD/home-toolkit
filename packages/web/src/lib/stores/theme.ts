8567dbe37581fd2a3446b5ab6827537427310a5c
Julien Zapata Duque <jzapata@live.nl>
2022-08-17 22:55:15 +0200

    feat(web): store theme preference in local storage

--- packages/web/src/lib/stores/theme.ts
+++ packages/web/src/lib/stores/theme.ts
@@ -1,3 +1,26 @@
+import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 
-export const isDark = writable(false);
+const themePrefKey = 'theme';
+
+const schema = '(prefers-color-scheme: dark)';
+
+const getInitialValue = () => {
+  const storedValue = localStorage.getItem(themePrefKey);
+
+  // If the user already set their theme once, simply return that
+  if (storedValue) {
+    return storedValue === 'dark';
+  }
+
+  // Default to preferred theme
+  return window.matchMedia(schema).matches;
+};
+
+export const isDark = writable(browser && getInitialValue());
+
+isDark.subscribe((newIsDark) => {
+  if (!browser) return;
+
+  localStorage.setItem(themePrefKey, newIsDark ? 'dark' : 'light');
+});
