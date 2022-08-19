b3dc8f267cc8ae4b60c8c7fa3046ad65f69bac4d
Julien Zapata Duque <jzapata@live.nl>
2022-08-19 14:18:42 +0200

    refactor(web): extract smart light component

---
+++ packages/web/src/lib/components/smart-home/lights/SmartLight.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Paper, Switch, Text } from '@svelteuidev/core';
+  import type { ISmartLight } from '@home-toolkit/types/smart-home';
+  import { smartHome } from '$lib/stores/smart-home/smart-home';
+
+  export let light: ISmartLight;
+
+  const toggleLight = (lightId: string) => {
+    smartHome.toggleLight(lightId);
+  };
+</script>
+
+<Paper my="xl">
+  <Text weight="bold" size="lg" mb="sm">{light.name}</Text>
+  <Switch on:click={() => toggleLight(light.id)} bind:checked={light.isOn} label="Enabled" />
+
+  <Text mt="sm" size="sm">Brightness: {light.brightness.toFixed()}</Text>
+</Paper>
--- packages/web/src/routes/smart-home/lights.svelte
+++ packages/web/src/routes/smart-home/lights.svelte
@@ -1,25 +1,14 @@
 <script lang="ts">
-  import { Box, Switch, Text } from '@svelteuidev/core';
   import { smartHome } from '$lib/stores/smart-home/smart-home';
-
-  const toggleLight = (lightId: string) => {
-    smartHome.toggleLight(lightId);
-  };
+  import SmartLight from '$lib/components/smart-home/lights/SmartLight.svelte';
 
   $: lightsLoaded = !!Object.keys($smartHome.lights).length;
-
-  // Looping over the store as is done below gives false positives
-  /* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
 </script>
 
 {#if !lightsLoaded}
   <p>Loading light data...</p>
 {:else}
-  {#each Object.entries($smartHome.lights) as [lightId, light]}
-    <Box my="xl">
-      <Text weight="bold">{light.name}</Text>
-      <Switch on:click={() => toggleLight(lightId)} bind:checked={light.isOn} label="Enabled" />
-      <Text mt="sm" size="sm">Brightness: {light.brightness.toFixed()}</Text>
-    </Box>
+  {#each Object.values($smartHome.lights) as light (light.id)}
+    <SmartLight {light} />
   {/each}
 {/if}
