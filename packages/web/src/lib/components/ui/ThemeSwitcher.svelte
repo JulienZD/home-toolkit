bf8c6f7105008e15594455fd04fa794607a92c91
Julien Zapata Duque <jzapata@live.nl>
2022-08-21 16:21:40 +0200

    feat(web): highlight currently active theme

--- packages/web/src/lib/components/ui/ThemeSwitcher.svelte
+++ packages/web/src/lib/components/ui/ThemeSwitcher.svelte
@@ -2,18 +2,37 @@
   import { themes } from '$lib/data/themes';
   import { onMount } from 'svelte';
   import { themeChange } from 'theme-change';
+  import { browser } from '$app/env';
+  import { ChevronDownIcon } from 'svelte-feather-icons';
+
+  let initialTheme: string;
 
   onMount(() => {
     themeChange(false);
+    if (!browser) return;
+
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      initialTheme = storedTheme;
+    }
   });
 </script>
 
 <div class="dropdown dropdown-end">
-  <span tabindex="0" class="btn btn-ghost rounded-btn">Theme</span>
+  <div tabindex="0" class="btn gap-1 btn-ghost">
+    <span>Theme</span>
+    <ChevronDownIcon size="16" />
+  </div>
+
   <ul tabindex="0" class="menu dropdown-content bg-base-200 p-2 shadow rounded-box w-52 mt-4 h-52 overflow-y-auto">
     {#each themes as theme}
       <li>
-        <button class="btn btn-sm btn-ghost" data-set-theme={theme.id}>{theme.name}</button>
+        <button
+          class="btn btn-sm btn-ghost"
+          class:active={initialTheme === theme.id}
+          data-set-theme={theme.id}
+          on:click={() => (initialTheme = theme.id)}>{theme.name}</button
+        >
       </li>
     {/each}
   </ul>
