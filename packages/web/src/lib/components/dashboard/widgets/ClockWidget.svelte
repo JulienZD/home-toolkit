b0c74760d31d5e1ff148a5b17b759801cbbc99cc
Julien Zapata Duque <jzapata@live.nl>
2022-08-27 22:07:34 +0200

    feat(web): add clock widget to dashboard

---
+++ packages/web/src/lib/components/dashboard/widgets/ClockWidget.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import DashboardWidget from './DashboardWidget.svelte';
+
+  let hour: number;
+  let minute: number;
+  let seconds: number;
+
+  const updateTime = () => {
+    const now = new Date();
+
+    hour = now.getHours();
+    minute = now.getMinutes();
+    seconds = now.getSeconds();
+  };
+
+  const prefixZeroIfNeeded = (digit: number) => (digit < 10 ? `0${digit}` : digit);
+
+  setInterval(updateTime, 1000);
+</script>
+
+<DashboardWidget px="0">
+  <div class="flex justify-center text-3xl min-w-[8rem]">
+    <span>{prefixZeroIfNeeded(hour) ?? '00'}</span>
+    <span>:</span>
+    <span>{prefixZeroIfNeeded(minute) ?? '00'}</span>
+    <span>:</span>
+    <span>{prefixZeroIfNeeded(seconds) ?? '00'}</span>
+  </div>
+</DashboardWidget>
--- packages/web/src/lib/components/dashboard/widgets/DashboardWidget.svelte
+++ packages/web/src/lib/components/dashboard/widgets/DashboardWidget.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   export let title = '';
+
+  export let px = '2';
+  export let py = '2';
 </script>
 
-<div class="rounded bg-base-200 p-2 flex flex-col">
+<div class={`rounded bg-base-200 px-${px} py-${py} flex flex-col`}>
   {#if title}
     <h2 class="mb-2">{title}</h2>
   {/if}
