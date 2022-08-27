ff5621fcf5cdd5fd31e3174a4d9128dcc5547001
Julien Zapata Duque <jzapata@live.nl>
2022-08-27 21:41:37 +0200

    fix(web): prevent widgets vertically stretching for no reason

--- packages/web/src/lib/components/dashboard/Dashboard.svelte
+++ packages/web/src/lib/components/dashboard/Dashboard.svelte
@@ -5,7 +5,7 @@
 
 <h1>Dashboard</h1>
 
-<div class="grid gap-2 grid-flow-row md:grid-flow-col">
+<div class="grid gap-2 items-start grid-flow-row md:grid-flow-col">
   <SmartLightsWidget />
   <WeatherWidget />
 </div>
